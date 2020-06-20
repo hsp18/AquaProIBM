ffc304d87fc3ec470eeb80e6b6e08d5de255f5d8
hsp18 <65398642+hsp18@users.noreply.github.com>
2020-06-20 19:38:50 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from flaskblog import app
 from flaskblog.routes import home
 import time
 if __name__ == '__main__':
-    app.run(debug=True,threaded=True)
+    app.run(debug=True,threaded=True,host='0.0.0.0')
